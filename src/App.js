e23d0a272763cfa1fc7ed8628563c8a8f8f67ae2
Cristian Villa <c.villavirgen@gmail.com>
2023-09-12 00:02:18 -0600

    Configure basic routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Sample component imports (I'll create these later)
+// import HomePage from './pages/HomePage';
+// import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {/* <HomePage /> */}
+          Home Page Placeholder
+        </Route>
+        <Route path="/details/:id">
+          {/* <DetailPage /> */}
+          Detail Page Placeholder
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
