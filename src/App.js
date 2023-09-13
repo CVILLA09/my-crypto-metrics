365a397535b4ec16822edf0a51178cc8f7892122
Cristian Villa <c.villavirgen@gmail.com>
2023-09-12 21:03:30 -0600

    Integrate Redux store and set up routing in App and index files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCryptoData } from './redux/cryptoSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  
+  // Fetch data when the component mounts
+  useEffect(() => {
+    dispatch(fetchCryptoData());
+  }, [dispatch]);
+
+  // Get data from Redux store
+  const cryptoData = useSelector((state) => state.crypto.cryptoData);
+  const status = useSelector((state) => state.crypto.status);
+
+  // Handle loading and error states
+  if (status === 'loading') return <p>Loading...</p>;
+  if (status === 'failed') return <p>Error fetching data.</p>;
+
   return (
     <Router>
       <Routes>
