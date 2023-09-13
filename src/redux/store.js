3ff72de460389da17345c4952f2eb3ce7ec81be3
Cristian Villa <c.villavirgen@gmail.com>
2023-09-13 10:35:38 -0600

    Extend Redux store to include detailSlice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import cryptoReducer from './cryptoSlice';
+import detailReducer from './detailSlice';
 
 const store = configureStore({
   reducer: {
     crypto: cryptoReducer,
+    detail: detailReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
