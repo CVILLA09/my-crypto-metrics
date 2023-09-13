db2fc9711743d9f9f19449efe4ff46bb9aba12b8
Cristian Villa <c.villavirgen@gmail.com>
2023-09-13 09:57:44 -0600

    Implement CryptoList component to display cryptocurrency details including name, symbol, and USD price

--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -1,26 +1,30 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 const CryptoList = () => {
-    const cryptoData = useSelector((state) => state.crypto.cryptoData.data);  // Note the added .data here
-    const status = useSelector((state) => state.crypto.status);
-  
-    console.log("Crypto Data:", cryptoData);  // Log the data
-  
-    if (status === 'loading') return <p>Loading...</p>;
-    if (status === 'failed') return <p>Error fetching data.</p>;
-  
-    return (
-      <div>
-        {cryptoData && Array.isArray(cryptoData) ? (
-          cryptoData.map((crypto, index) => (
-            <div key={index}>
-              <h3>{crypto.name}</h3>
-              <p>Symbol: {crypto.symbol}</p>
-              <p>Price (USD): {crypto.rateUsd ? parseFloat(crypto.rateUsd).toFixed(2) : 'N/A'}</p>
-            </div>
-          ))
-        ) : (
-          <p>No data available</p>
-        )}
-      </div>
-    );
-  };
-  
+  const cryptoData = useSelector((state) => state.crypto.cryptoData.data);
+  const status = useSelector((state) => state.crypto.status);
+
+  console.log("Crypto Data:", cryptoData);  // Log the data for debugging
+
+  if (status === 'loading') return <p>Loading...</p>;
+  if (status === 'failed') return <p>Error fetching data.</p>;
+
+  return (
+    <div>
+      {cryptoData && Array.isArray(cryptoData) ? (
+        cryptoData.map((crypto, index) => (
+          <div key={index}>
+            <h3>{crypto.name}</h3>
+            <p>Symbol: {crypto.symbol}</p>
+            <p>Price (USD): {typeof crypto.priceUsd === 'number' ? crypto.priceUsd.toFixed(2) : parseFloat(crypto.priceUsd).toFixed(2)}</p>
+          </div>
+        ))
+      ) : (
+        <p>No data available</p>
+      )}
+    </div>
+  );
+};
+
+export default CryptoList;
