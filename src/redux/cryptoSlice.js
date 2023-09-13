a8d65aadb409d91be565b521170d3b4eb0f25153
Cristian Villa <c.villavirgen@gmail.com>
2023-09-12 22:11:13 -0600

    Switch to CoinCap API for fetching crypto data

--- src/redux/cryptoSlice.js
+++ src/redux/cryptoSlice.js
@@ -4,17 +4,7 @@ import axios from 'axios';
 export const fetchCryptoData = createAsyncThunk(
   'crypto/fetchCryptoData',
   async () => {
-    const API_KEY = '2a5b5dfc-d30c-4292-ab84-66794c11ce94';
-    const response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
-      headers: {
-        'X-CMC_PRO_API_KEY': API_KEY,
-      },
-      params: {
-        start: 1,
-        limit: 10,
-        convert: 'USD',
-      },
-    });
+    const response = await axios.get('https://api.coincap.io/v2/assets?limit=10');
     return response.data;
   },
 );
