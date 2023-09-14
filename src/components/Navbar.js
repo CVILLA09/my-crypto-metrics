356c87f6f19c7f64ddb06316ac9157ade4564065
Cristian Villa <c.villavirgen@gmail.com>
2023-09-14 00:20:10 -0600

    Update styles in Navbar.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import 'font-awesome/css/font-awesome.min.css';
+import './Navbar.css';
 
 const Navbar = () => (
   <div className="navbar">
-    <span className="left-arrow">&#60;</span>
-    <span className="navbar-title">Crypto Currencies</span>
+    <i className="fa fa-arrow-left left-arrow" />
+    <span className="navbar-title">my crypto metrics</span>
     <span className="navbar-icons">
       <i className="fa fa-microphone mic-icon" />
       <i className="fa fa-cog gear-icon" />
