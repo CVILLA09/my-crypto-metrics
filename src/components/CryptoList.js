eb7aa824a88794df4c88b606763dbcfce5042006
Cristian Villa <c.villavirgen@gmail.com>
2023-09-14 03:15:02 -0600

    Implement styles for CryptoList component

--- src/components/CryptoList.js
+++ src/components/CryptoList.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { filterCryptos } from '../redux/cryptoSlice';
+import './CryptoList.css';
 
 const CryptoList = () => {
   const dispatch = useDispatch();
@@ -25,37 +26,45 @@ const CryptoList = () => {
   return (
     <div>
       <div className="searchContainer">
-        <h2>Search Crypto Here</h2>
         <input
+          className="searchBar"
           type="text"
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
           onKeyDown={handleKeyDown}
-          placeholder="Search by name"
+          placeholder="Curious about crypto? Type a name and let's dive in!"
         />
-        <button type="button" onClick={handleSearch}>
-          Search
+        <button className="searchBtn" type="button" onClick={handleSearch}>
+          <i className="fa fa-search" />
         </button>
       </div>
-      {cryptoData && Array.isArray(cryptoData) ? (
-        cryptoData.map((crypto) => (
-          <div key={crypto.id}>
-            <Link to={`/details/${crypto.id}`}>
-              <h3>{crypto.name}</h3>
-            </Link>
-            <p>
-              Symbol:
-              {crypto.symbol}
-            </p>
-            <p>
-              Price (USD):
-              {typeof crypto.priceUsd === 'number' ? crypto.priceUsd.toFixed(2) : parseFloat(crypto.priceUsd).toFixed(2)}
-            </p>
-          </div>
-        ))
-      ) : (
-        <p>No data available</p>
-      )}
+      <div className="coinsList">
+        {cryptoData && Array.isArray(cryptoData) ? (
+          cryptoData.map((crypto, index) => (
+            <div
+              key={crypto.id}
+              className="coinCard"
+              style={{
+                backgroundColor: ((Math.floor(index / 2) + index) % 2 === 0) ? '#4369B2' : '#3B5D9F',
+              }}
+            >
+              <Link to={`/details/${crypto.id}`}>
+                <h3>{crypto.name}</h3>
+              </Link>
+              <p>
+                Symbol:
+                {crypto.symbol}
+              </p>
+              <p>
+                Price (USD):
+                {typeof crypto.priceUsd === 'number' ? crypto.priceUsd.toFixed(2) : parseFloat(crypto.priceUsd).toFixed(2)}
+              </p>
+            </div>
+          ))
+        ) : (
+          <p>No data available</p>
+        )}
+      </div>
     </div>
   );
 };
