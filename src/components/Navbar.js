2f74bf0672db3bc8291f76de90d2180d35da1f17
Cristian Villa <c.villavirgen@gmail.com>
2023-09-14 20:50:23 -0600

    Add 'Back' button functionality and clear selected details in Redux

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,29 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { clearSelectedDetails } from '../redux/detailSlice';
 import 'font-awesome/css/font-awesome.min.css';
 import './Navbar.css';
 
-const Navbar = () => (
-  <div className="navbar">
-    <i className="fa fa-arrow-left left-arrow" />
-    <span className="navbar-title">my crypto metrics</span>
-    <span className="navbar-icons">
-      <i className="fa fa-microphone mic-icon" />
-      <i className="fa fa-cog gear-icon" />
+const Navbar = () => {
+  const dispatch = useDispatch();
 
-    </span>
-  </div>
-);
+  const handleGoBack = () => {
+    dispatch(clearSelectedDetails());
+  };
+
+  return (
+    <div className="navbar">
+      <NavLink to="/" onClick={handleGoBack}>
+        <i className="fa fa-arrow-left left-arrow" />
+      </NavLink>
+      <span className="navbar-title">my crypto metrics</span>
+      <span className="navbar-icons">
+        <i className="fa fa-microphone mic-icon" />
+        <i className="fa fa-cog gear-icon" />
+      </span>
+    </div>
+  );
+};
 
 export default Navbar;
